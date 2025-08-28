a63ad0c0d4292ba3afb3c3e1e571d09d703c52ac
SahejTheWarrior <sahejburande@gmail.com>
2025-08-28 11:37:43 +0100

    Add files via upload

---
+++ activity2_5.py
@@ -0,0 +1,25 @@
+#not operator
+
+s=10
+d=12
+g=12
+
+print(s !=d)
+print(d !=g)
+
+s="python"
+d="coding"
+
+if s !=d:
+    print(s, "and",d, "are different")
+
+s=4
+d=5
+
+if (s==1)!=(d==5):
+    print("hello")
+
+s=int(input("enter a number"))
+
+if s%2 !=0:
+    print(s,"is not even number")
